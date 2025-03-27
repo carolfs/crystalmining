06d33a5bfa35ba97a35b8dba1eca6c4128e59b6a
Carolina Feher da Silva <carolfsu@gmail.com>
2025-03-27 13:49:53 +0000

    Fixed testing

--- ecocredits/task.js
+++ ecocredits/task.js
@@ -152,12 +152,12 @@ function start_experiment() {
     document.querySelector("#ethics").remove();
     document.body.className = "running";
     
-    // run_instructions(
-    //     null,
-    //     document.querySelector("#tutorial-instructions"),
-    //     function(last_page) {
-    //         run_tutorial(last_page);
-    //     });
+    run_instructions(
+        null,
+        document.querySelector("#tutorial-instructions"),
+        function(last_page) {
+            run_tutorial(last_page);
+        });
     // run_instructions(
     //         null,
     //         document.querySelector("#quiz-instructions"),
@@ -166,7 +166,7 @@ function start_experiment() {
     //         });
     // run_quiz(null);
     // run_trials(null, false, show_feedback);
-    show_feedback(100);
+    // show_feedback(100);
 }
 
 function substitute_constants() {
